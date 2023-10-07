f950328cbae9c578e80cfc682f4948e306228a5e
Raymond <70726900+ray165@users.noreply.github.com>
2023-10-06 23:26:36 -0700

    Working locally now updated to render url

--- COMP4537/labs/4/app.js
+++ COMP4537/labs/4/app.js
@@ -4,6 +4,10 @@ const qs = require('querystring');
 
 // Create an array to store word definitions
 const dictionary = [];
+const headers = { 
+  'Content-Type': 'application/json',
+  "Access-Control-Allow-Origin": "*"
+};
 
 // Function to handle POST requests
 function handlePostRequest(request, response) {
@@ -19,16 +23,16 @@ function handlePostRequest(request, response) {
     const definition = parsedData.definition;
 
     if (!word || !definition) {
-      response.writeHead(400, { 'Content-Type': 'application/json' });
+      response.writeHead(400, headers);
       response.end(JSON.stringify({ message: 'Word and definition are required fields.' }));
     } else {
       const existingEntry = dictionary.find((entry) => entry.word === word);
       if (existingEntry) {
-        response.writeHead(409, { 'Content-Type': 'application/json' });
+        response.writeHead(409, headers);
         response.end(JSON.stringify({ message: `${word} already exists.` }));
       } else {
         dictionary.push({ word, definition });
-        response.writeHead(201, { 'Content-Type': 'application/json' });
+        response.writeHead(201, headers);
         response.end(JSON.stringify({ message: `Request #${dictionary.length}`, entry: `${word}: ${definition}` }));
       }
     }
@@ -41,15 +45,15 @@ function handleGetRequest(request, response) {
   const word = query.word;
 
   if (!word) {
-    response.writeHead(400, { 'Content-Type': 'application/json' });
+    response.writeHead(400, headers);
     response.end(JSON.stringify({ message: 'Word parameter is required for GET request.' }));
   } else {
     const entry = dictionary.find((entry) => entry.word === word);
     if (entry) {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.writeHead(200, headers);
       response.end(JSON.stringify({ word, definition: entry.definition }));
     } else {
-      response.writeHead(404, { 'Content-Type': 'application/json' });
+      response.writeHead(404, headers);
       response.end(JSON.stringify({ message: `Request #${dictionary.length + 1}, word '${word}' not found.` }));
     }
   }
