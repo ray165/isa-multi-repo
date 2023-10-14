f6cb773a5c7cc468f6891ffec2df9c65443d3e18
Deji Toki <adedeji.toki@outlook.com>
2023-10-14 15:09:00 -0700

    Get sql commands working

--- COMP4537/labs/5/server2/server.js
+++ COMP4537/labs/5/server2/server.js
@@ -3,32 +3,67 @@
 const http = require('http');
 const mysql = require('mysql');
 const url = require('url');
+var pg = require('pg');
+require('dotenv').config();
 
+// my sql create table
+// const create_table = "CREATE TABLE IF NOT EXISTS patients (\
+//     id SERIAL PRIMARY KEY,\
+//     name VARCHAR(100) NOT NULL,\
+//     dateOfBirth DATETIME";
 const create_table = "CREATE TABLE IF NOT EXISTS patients (\
-    id MEDIUMINT NOT NULL AUTO_INCREMENT,\
+    id SERIAL PRIMARY KEY,\
     name VARCHAR(100) NOT NULL,\
-    dateOfBirth DATETIME,\
-    PRIMARY KEY (id)";
+    dateOfBirth DATE\
+)";
 
-const db = mysql.createConnection({
-    host: 'sql.freedb.tech',
-    user: 'freedb_rayadmin',
-    password: 'your_password',  // add env variable
-    database: 'freedb_lab5-db',
-    port: 3306
+var conString = process.env.ELPHA_URL //Can be found in the Details page
+var db = new pg.Client(conString);
+
+// my sql connection
+// const db = mysql.createConnection({
+//     host: 'sql.freedb.tech',
+//     user: process.env.SQL_USER || 'freedb_rayadmin',
+//     password: process.env.SQL_PASS || 'your_password',  // add env variable
+//     database: 'freedb_lab5-db',
+//     port: 3306
+// });
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database.');
 });
 
+const headers = { 
+    'Content-Type': 'text/plain',
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": '*'
+  };
+
 const server = http.createServer((req, res) => {
+    const { method } = req;
+    const execute = true;
     const parsedUrl = url.parse(req.url);
     const pathname = parsedUrl.pathname;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
 
-    if (req.method === 'POST' && pathname  === '/api/v1/sql/insertDefault') {
-        handleInsertDefault(req, res);
-    } else if (req.method === 'GET' || req.method === 'POST' && pathname === '/api/v1/sql/runQuery') {
-        // something
-    } else if (req.method === 'GET' && pathname === '/api/v1/sql/readAll') {
-        handleReadAll(req, res);
-    } else {
+    console.log(req.method);
+    console.log(pathname);
+    console.log('body' + req.body);
+
+    if (req.method === 'POST') {
+        console.log("execute");
+        handleExecute(req, res);
+    }
+    else if (req.method === 'OPTIONS') {
+        res.writeHead(200);
+        res.end("Server is running");
+        return;
+    }
+    else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Not Found');
     }
@@ -41,14 +76,10 @@ var handleInsertDefault = (req, res) => {
     });
 
     req.on('end', () => {
-        try {
-            const data = JSON.parse(body);
-            const query = data.query;
-            handleQuery(res, query);
-        } catch (error) {
-            res.writeHead(400, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ error: 'Invalid JSON data' }));
-        }
+        const data = JSON.parse(body);
+        const query = data.query;
+        console.log(query);
+        handleQuery(res, query);
     });
 }
 
@@ -64,9 +95,37 @@ var handleReadAll = (req, res) => {
     });
 }
 
-const handleQuery = (res, query) => {
-    db.connect();
+var handleExecute = (req, res) => {
+    if (req.method === 'POST') {
+        console.log("POST Method running");
+            let body = '';
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
 
+        req.on('end', () => {
+            const data = JSON.parse(body);
+            const query = data.query;
+            console.log(query);
+            handleQuery(res, query);
+        });
+    }
+    else if (req.method === 'GET'){
+        console.log("GET Method running");
+        console.log(req.url);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end("GET Request");
+        return
+    }
+}
+
+const handleQuery = (res, query) => {
+    console.log("handleQuery");
+    console.log(query);
+    console.log("dbPAss: " + process.env.SQL_PASS)
+    
+
+    // Create default table
     db.query(create_table, (err, results) => {
         if (err) {
             console.error(err);
@@ -75,7 +134,12 @@ const handleQuery = (res, query) => {
         }
     });
 
+    // handle query
     db.query(query, (error, results) => {
+        console.log("db.query results");
+        console.log(error);
+        console.log(results);
+
         if (error) {
             res.writeHead(500, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ error: error.message }));
@@ -83,8 +147,6 @@ const handleQuery = (res, query) => {
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify(results));
         }
-
-        db.end();
     });
 }
 
